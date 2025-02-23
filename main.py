f2a357fd92425536e6b70abbbcba98a8174111bf
Kyle-Mendoza <kimmendoza0131@gmail.com>
2025-02-24 00:46:17 +0800

    BB-6: Add report

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ with open(open_to_file_path) as f: # with for opening a file, open*(file_url_to_
     file_contents = f.read()
 
     word_count = count_words(file_contents)
-    
+    characters_count = count_characters(file_contents)
+
     print(f"{word_count} words found in the document")
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")    
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    
+    for character in characters_count:
+        if character.isalpha(): 
+            print(f"{character}: {characters_count[character]}") 
